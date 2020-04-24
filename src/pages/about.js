f138a145b30f8f943143ea601c9ede136f01efd2
Eduard Castro <eduard.cas07@gmail.com>
2020-04-23 20:45:04 -0500

    Add static pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default () => (
     <div>
-        <h1>This is the index page</h1>
+        <h1>This is the about page</h1>
 
         <p>Non Lorem enim sint nisi anim ad qui consectetur nisi nulla sit.</p>
     </div>
