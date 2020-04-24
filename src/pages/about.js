464b2f220dd8bc7d84cda31b885fe147c59b2b84
Eduard Castro <eduard.cas07@gmail.com>
2020-04-23 20:52:35 -0500

    Add linking between pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,13 @@
-import React from "react"
+import React from 'react'
+import { Link } from 'gatsby'
 
 export default () => (
     <div>
         <h1>This is the about page</h1>
+
+        <div>
+            <Link to='/'>Home</Link> | <Link to='/about'>About</Link>
+        </div>
 
         <p>Non Lorem enim sint nisi anim ad qui consectetur nisi nulla sit.</p>
     </div>
