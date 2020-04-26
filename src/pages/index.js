331cb0f2101d89cb3528347d9e417cc53a8ecffc
Eduard Castro <eduard.cas07@gmail.com>
2020-04-25 21:19:08 -0500

    Update pages component and layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import Title from '../components/title'
 
 export default () => (
     <Layout>
-        <h1>This is the index page</h1>
-
+        <Title text='Welcome' />
         <div>
-            <Link to='/'>Home</Link> | <Link to='/about'>About</Link>
+            <Link to='/'>Home</Link> | <Link to='/about'>About me</Link>
         </div>
-
-        <p>Non Lorem enim sint nisi anim ad qui consectetur nisi nulla sit.</p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
     </Layout>
 )
