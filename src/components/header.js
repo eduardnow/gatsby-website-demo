40a3dc15adae73f07b0f848ccd8e2d30afe938e3
Eduard Castro <eduard.cas07@gmail.com>
2020-04-25 21:08:53 -0500

    Add header links and social buttons

--- src/components/header.js
+++ src/components/header.js
@@ -14,9 +14,43 @@ const HomeButton = props => (
   </Link>
 )
 
+// SocialButton component
+const SocialButton = (props) => {
+
+  let style = '';
+  let url = '';
+
+  if (props.site === 'twitter') {
+    style = styles.buttonTwitter;
+    url = "https://twitter.com/" + props.username;
+  }
+  else if (props.site === 'linkedin') {
+    style = styles.buttonLinkedin;
+    url = "https://www.linkedin.com/in/" + props.username;
+  }
+  else if (props.site === 'github') {
+    style = styles.buttonGithub;
+    url = "https://www.github.com/" + props.username;
+  }
+
+  return (
+    <a href={url} target="_blank" rel="noopener noreferrer">
+      <div className={style}>{props.children}&nbsp;</div>
+    </a>
+  )
+
+}
+
 export default () => (
 
   <header className={styles.container}>
+
+    <div className={styles.row}>
+      <HomeButton to='/' text='My Gatsby blog' />
+      <SocialButton site="github" username="evangeloper"></SocialButton>
+      <SocialButton site="linkedin" username="evangeloper"></SocialButton>
+      <SocialButton site="twitter" username="evangeloper"></SocialButton>
+    </div>
 
     <div className={styles.row}>
       <HeaderLink to='/' text='ARTICLES' />
